bde196833907dfc5dc19a0c8c2f379fd48cd02af
yimkong <17665448577@163.com>
2020-07-22 08:45:55 +0800

    onclick event add html and modify css

---
+++ js_learning/tool/debug.js
@@ -0,0 +1,13 @@
+function debug(msg) {
+    var log = document.getElementById("debuglog");
+    if (!log) {
+        log = document.createElement("div");
+        log.id = "debuglog";
+        log.innerHTML="<h1>Debug Log</h1>";
+        document.body.appendChild(log);
+    }
+    var pre = document.createElement("pre");
+    var text = document.createTextNode(msg);
+    pre.appendChild(text);
+    log.appendChild(pre);
+}
